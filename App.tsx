8774e43dde97c3d6735704cf7ecd769cd6056813
chunix <linoxolus@gmail.com>
2024-10-17 04:43:41 +0000

    initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text>DMCS</Text>
+    </View>
+  );
+}
+
+export default App;
